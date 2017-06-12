dfa5fa05496867c1a501d10e58a6e5bf7dcd510d
Brady <bradyehahn@icloud.com>
2017-06-12 09:29:20 -0500

    Updated Commands

--- java/me/zero/example/command/CommandManager.java
+++ java/me/zero/example/command/CommandManager.java
@@ -1,7 +1,11 @@
 package me.zero.example.command;
 
+import me.zero.client.api.ClientAPI;
 import me.zero.client.api.command.Command;
+import me.zero.client.api.command.handler.CommandHandler;
+import me.zero.client.api.command.handler.listener.ChatCommandListener;
 import me.zero.client.api.manage.Manager;
+import me.zero.example.ExampleClient;
 import me.zero.example.command.commands.TestCommand;
 
 /**
@@ -9,13 +13,23 @@ import me.zero.example.command.commands.TestCommand;
  */
 public final class CommandManager extends Manager<Command> {
 
+    private final CommandHandler handler = new CommandHandler(ExampleClient.getInstance());
+
     public CommandManager() {
         super("Command");
     }
 
     @Override
     public void load() {
-        this.addData(new TestCommand());
+        this.addData(
+                new TestCommand()
+        );
+
+        // Setup the handler and a chat command listener
+        ClientAPI.EVENT_BUS.subscribe(
+                handler,
+                new ChatCommandListener(handler)
+        );
     }
 
     @Override
--- java/me/zero/example/command/commands/TestCommand.java
+++ java/me/zero/example/command/commands/TestCommand.java
@@ -3,20 +3,23 @@ package me.zero.example.command.commands;
 import me.zero.client.api.command.Cmd;
 import me.zero.client.api.command.Command;
 import me.zero.client.api.command.exception.CommandException;
-import net.minecraft.client.network.NetworkPlayerInfo;
+import me.zero.client.api.command.executor.sender.CommandSender;
 import net.minecraft.util.text.TextComponentString;
+
+import java.util.Arrays;
 
 /**
  * @author Brady
  * @since 5/31/2017 6:18 PM
  */
-@Cmd(headers = { "test", "me/zero/example"}, description = "Test Command")
+@Cmd(headers = { "test", "example"}, description = "Test Command")
 public final class TestCommand extends Command {
 
     @Override
-    public void execute(NetworkPlayerInfo sender, String[] arguments) throws CommandException {
+    public void execute(CommandSender sender, String[] arguments) throws CommandException {
+        // Print a chat message indicating the success of the
         mc.ingameGUI.getChatGUI().printChatMessage(new TextComponentString(
-                String.format("%s executed the \"Test\" command!", sender.getGameProfile().getName())
+                String.format("%s executed the \"Test\" command with arguments %s", sender.getName(), Arrays.toString(arguments))
         ));
     }
 }
